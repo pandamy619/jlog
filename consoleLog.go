9413182d04c961c96687939a08dc672f03a2a825
Тагаев Владислав <tagaev@qlever.ru>
2020-06-02 16:19:44 +0300

    fixed console log

--- consoleLog.go
+++ consoleLog.go
@@ -10,7 +10,7 @@ func (l *Logs) typeConsoleLog(prefix string, message string) {
 	case "simple":
 		l.simple(l.Pkg, l.name, prefix, message)
 	case "json":
-		l.json(l.Pkg, l.name)
+		l.json(l.Pkg, l.name, prefix, message)
 	default:
 		l.simple(l.Pkg, l.name, prefix, message)
 	}
@@ -18,7 +18,7 @@ func (l *Logs) typeConsoleLog(prefix string, message string) {
 
 func (l *Logs) simple(pkg string, function string, status string, message string) {
 	datetime := time.Now().Format("2006-01-02T15:04:05")
-	fmt.Printf("%s \u001b[32mPACKAGE\u001B[0m:%s->\u001B[33;1mFUNC\u001B[0m:%s %s %s\n",
+	fmt.Printf("%s \u001b[32m[package]\u001B[0m %s \u001B[33;1m[func]\u001B[0m %s %s %s\n",
 		datetime,
 		pkg,
 		function,
@@ -27,8 +27,9 @@ func (l *Logs) simple(pkg string, function string, status string, message string
 		)
 }
 
-func (l *Logs) json(pkg string, function string) {
-	fmt.Printf("\u001B[32mpackage\u001B[0m: %s \u001B[33;1mfunc\u001B[0m: %s\n", pkg, function)
+func (l *Logs) json(pkg string, function string, prefix string, message string) {
+	fmt.Printf("\u001B[32m[package]\u001B[0m %s \u001B[33;1m[func]\u001B[0m %s\n", pkg, function)
+	fmt.Printf("%s %s\n", prefix, message)
 	fmt.Printf("%s", string(structJson(l.fields)))
 	fmt.Printf("\n")
 }
