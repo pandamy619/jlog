c0414ea9874a9ce36eff314db72a38c98d3e10a6
Тагаев Владислав <tagaev@qlever.ru>
2020-09-29 21:41:49 +0300

    feat(filemanager)

--- status.go
+++ status.go
@@ -1,18 +1,6 @@
 package jlog
 
-import (
-	"fmt"
-	"os"
-	"time"
-)
-
-func (l *Jlogs) makePath() string {
-	return fmt.Sprintf("%s/%s", l.location, l.Pkg)
-}
-
-func (l *Jlogs) makeFilename() string {
-	return fmt.Sprintf("%s/%s/%s.json", l.location, l.Pkg, time.Now().Format("2006-01-02T15:04:05"))
-}
+import "os"
 
 func (l *Jlogs) Info(message string) {
 	l.typeConsoleLog(infoPrefix, message)
--- utils.go
+++ utils.go
@@ -3,8 +3,6 @@ package jlog
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"os"
 )
 
 func structJson(obj interface{}) []byte{
@@ -13,18 +11,4 @@ func structJson(obj interface{}) []byte{
 		fmt.Println(err)
 	}
 	return o
-}
-
-// Creates a directory named path
-func createDirs(path string) {
-	_, err := os.Stat(path)
-	if os.IsNotExist(err) {
-		_ = os.MkdirAll(path, 0777)
-	}
-}
-
-// Creates a directory named path and save file to this directory
-func outFile(path string, filename string, makeData []byte) {
-	createDirs(path)
-	_ = ioutil.WriteFile(filename, makeData,0777)
 }
