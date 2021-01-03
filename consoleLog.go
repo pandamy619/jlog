a83ed3b13f905665a454166ad8526536073f93ac
vladislav.tagayev <vladislav.tagayev@nedra.digital>
2021-01-04 02:47:57 +0300

    rename methods that name package and function. Add comment for this methos

--- consoleLog.go
+++ consoleLog.go
@@ -37,23 +37,26 @@ func (l *Jlogs) sep(status string) string {
 	return fmt.Sprintf("%s-------------------------%s", color, resetColor)
 }
 
-func (l *Jlogs) pkgRow() string {
+// getPkg return colorized name package.
+func (l *Jlogs) getPkg() string {
 	return pkgPrefix + ":" + brightGreen + l.Pkg + resetColor
 }
 
-func (l *Jlogs) funcRow() string {
+// getFunc return colorized name function.
+func (l *Jlogs) getFunc() string {
 	return funcPrefix + ":" + brightGreen + l.name + resetColor
 }
 
-func (l *Jlogs) detailRow(message string) string {
+// addPrefix message then add colorized prefix 'detail' to the message and return it.
+func (l *Jlogs) addPrefix(message string) string {
 	return detailPrefix + ":" + message
 }
 
-// simple calls Println to print row to the console
-// row is formed from status/name package/name function/message
+// simple calls Println to print row to the console.
+// Row is formed from status/name package/name function/message.
 func (l *Jlogs) simple(prefix string, message string) {
 	fmt.Println(
-		fmt.Sprintf("%s %s %s %s", prefix, l.pkgRow(), l.funcRow(), l.detailRow(message)),
+		fmt.Sprintf("%s %s %s %s", prefix, l.getPkg(), l.getFunc(), l.addPrefix(message)),
 	)
 }
 
@@ -61,7 +64,7 @@ func (l *Jlogs) json(prefix string, message string) {
 	status := strings.ToLower(l.fields["status"].(string))
 	sep := l.sep(status)
 	fmt.Printf("%s\n", sep)
-	fmt.Printf("%s %s\n%s\n%s\n", prefix, message, l.pkgRow(), l.funcRow())
+	fmt.Printf("%s %s\n%s\n%s\n", prefix, message, l.getPkg(), l.getFunc())
 	fmt.Printf("%s\n", string(convertToJson(l.fields)))
 	fmt.Printf("%s\n", sep)
 }
